716a8fc78d414904d7dd23129adf1ee7834f1a1e
Damian Silbergleith Cunniff <dcunniff@gmail.com>
2018-04-30 07:23:19 -0700

    customize: initial functionality for config files
    * Can specify `--stage=test` when using `yarn` to switch environs
    * `dev` is default environ if unspecified
      * dev
      * test
      * stage
      * prod
    * stage/prod are empty files right now
    * running `npm test` doesn't load the environments, tests fail

--- src/test-func/handler.js
+++ src/test-func/handler.js
@@ -2,7 +2,7 @@ const hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
+      message: `Go Serverless v1.0! Your function executed successfully! ENV: ${process.env.SOMEVAL}`,
       input: event,
     }),
   };

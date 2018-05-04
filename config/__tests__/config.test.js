fd3f1dd5239d0188ae9076f60c7a1acc5bc2be16
Damian Silbergleith Cunniff <dcunniff@gmail.com>
2018-05-04 07:33:08 -0700

    customize: Add tests to prove region and stage, update configs

--- config/__tests__/config.test.js
+++ config/__tests__/config.test.js
@@ -19,4 +19,26 @@ describe('Config Files', () => {
   test('prod config to be correct', () => {
     expect(prodConfig).toMatchSnapshot();
   });
+
+  test('dev has correct stage and region', () => {
+    expect(devConfig.stage).toBe('dev');
+    expect(devConfig.region).toBe('us-east-1');
+  });
+
+  test('test has correct stage and region', () => {
+    expect(testConfig.stage).toBe('test');
+    expect(testConfig.region).toBe('us-east-1');
+  });
+
+  test('stage has correct stage and region', () => {
+    expect(stageConfig.stage).toBe('stage');
+    expect(stageConfig.region).toBe('us-east-1');
+  });
+
+  test('prod has correct stage and region', () => {
+    expect(prodConfig.stage).toBe('prod');
+    expect(prodConfig.region).toBe('us-east-1');
+  });
+
 });
+
